734c8346520f89533f43801ec0d035fbfff7e47c
huangruntian <runtian.huang@tcl.com>
2024-03-04 10:05:12 +0800

    <修改><完善pom文件>

---
+++ src/main/java/com/hrt/example/SimpleSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.hrt.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrt/example/SimpleSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrt.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
